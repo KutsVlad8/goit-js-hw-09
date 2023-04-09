86cae91833420778b5be190ff0bba39e60d146bb
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-04-09 15:43:11 +0300

    update task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -41,8 +41,6 @@ const flatPick = flatpickr('input#datetime-picker', options);
 
 refs.startBtn.addEventListener('click', startTimer);
 
-const intervalId = setInterval(timeToEndTimer, 1000);
-
 function startTimer() {
   const intervalId = setInterval(timeToEndTimer, 1000);
 }
